4dcae5afdac4ced5aced5a8ad36c0d6c3ba36f8d
slowy07 <slowy.arfy@gmail.com>
2023-08-01 00:53:35 +0700

    fix: testing timer
    according #4
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- clara_test/tests/classFunction/timer.cpp
+++ clara_test/tests/classFunction/timer.cpp
@@ -5,19 +5,6 @@
 #include "gtest/gtest.h"
 
 using namespace clara;
-TEST(clara_Timer_get_duration, AllTests) {
-  using namespace std::chrono;
-
-  Timer<> t1;
-  std::this_thread::sleep_for(seconds(1));
-  t1.toc();
-
-  auto duration_t1_s = t1.get_duration();
-  EXPECT_NEAR(duration_t1_s.count(), 1, 0.05);
-
-  auto duration_t1_ms = t1.get_duration<milliseconds>();
-  EXPECT_NEAR(duration_t1_ms.count(), 1000, 50);
-}
 
 TEST(clara_timer, AllTests) {
   using namespace std::chrono;
