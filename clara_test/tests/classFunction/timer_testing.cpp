365ae9be9cc5259c309b4b6de79a3d75d5d9d49d
slowy07 <slowy.arfy@gmail.com>
2023-08-08 13:58:10 +0700

    fix: testing timer
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- clara_test/tests/classFunction/timer_testing.cpp
+++ clara_test/tests/classFunction/timer_testing.cpp
@@ -6,28 +6,16 @@
 
 using namespace clara;
 
-const double TOLERANCE = 1e-6;
-
-class TimerTest : public ::testing::Test {
-protected:
-  void sleep_for(int milliseconds) {
-std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
-  }
-};
-
-TEST_F(TimerTest, BasicFunctionallyTest) {
-  Timer<std::chrono::milliseconds> timer;
-  sleep_for(100);
-  timer.toc();
-  EXPECT_NEAR(timer.tics(), 100.0, TOLERANCE);
+TEST(clara_timer_test, DefaultConstructor) {
+  Timer<> timer;
+  EXPECT_GE(timer.tics(), 0);
 }
 
-TEST(clara_timer, AllTests) {
-  using namespace std::chrono;
-
-  Timer<> t;
-  std::this_thread::sleep_for(milliseconds(100));
-  t.tic();
-  t.toc();
-  EXPECT_NEAR(t.tics(), 0, 0.05);
+TEST(clara_timer_test, TicToc) {
+  Timer<> timer;
+  timer.tic();
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  timer.toc();
+  auto duration = timer.get_duration();
+  EXPECT_GE(duration.count(), 2);
 }
