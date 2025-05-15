4084aa4b8eb47e93dd23005a0ffc2f6b05c80721
slowy07 <slowy.arfy@proton.me>
2025-05-15 18:37:04 +0700

    chore: update template base singleton class
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- include/internal/classFunction/singleton.h
+++ include/internal/classFunction/singleton.h
@@ -26,43 +26,77 @@ namespace clara {
 namespace internal {
 
 /**
- * @class Singleton
- * @brief Implementation of a singleton pattern
- * @tparam T the type of the class that needs to be a singleton. the class should have
- *         a private constructor and descructor
+ * @brief template singleton class to enforce single instance of type T
+ *
+ * this class provides a thread-safe way to manage a singleton object
+ * it support both global (shared across threads) and thread-local instances,
+ * depending on whether NO_THREAD_LOCAL is defined
  */
 template <typename T>
 class Singleton {
- protected:
-  Singleton() noexcept = default;
-  Singleton(const Singleton&) = delete;
-  Singleton& operator=(const Singleton&) = delete;
-  virtual ~Singleton() = default;
-
  public:
   /**
-   * @brief get the instance of the singleton class
-   * @return reference to the singleton instance
-   * NOTE: the first call to this function will construct the singleton instance
-   *       subsequemt calls will return the same instance.
+   * @brief delete copy constructor and assignment operator
+   *
+   * prevent copying of the singleton instance to make sure only one exists
    */
-  static T& get_instance() noexcept(std::is_nothrow_constructible<T>::value) {
-    static T instance;
-    return instance;
-  }
-#ifndef NO_THREAD_LOCAL_
+  Singleton(const Singleton&) = delete;
+  Singleton& operator=(const Singleton&) = delete;
+
   /**
-   * @brief get the thread-local instance of the singleton class
-   * @return reference to the thread-local singleton instance
-   * NOTE: the first class to this function in each thread will construct a separate
-   *       instance for that thread. subsequent  calls in the same thread will return
-   *       the same thread-local instance
+   * get global (non-thread-local) singleton instance
+   *
+   * usign static local variable which is initialized once and shared globally
+   * thread safe in C++11 and later due guaranteed initialization order
    */
-  static T& get_thread_local_instance() noexcept(std::is_nothrow_constructible<T>::value) {
-    thread_local static T instance;
+  static T& get_no_thread_local_instance() noexcept(std::is_nothrow_constructible_v<T>) {
+    static T instance{};
     return instance;
   }
-#endif  // !NO_THREAD_LOCAL_
+
+#ifndef NO_THREAD_LOCAL
+  /**
+   * @brief get thread-local singleton instance
+   *
+   * each thread gets its own unique instance of T
+   * avoids synchronization overhead and race condition between threads
+   */
+  static T& get_thread_local_instance() noexcept(std::is_nothrow_constructible_v<T>) {
+    thread_local static T instance{};  // one instance per thread
+    return instance;
+  }
+#endif  // !NO_THREAD_LOCAL
+
+  /**
+   * @brief get the appropriate singleton instance based on configuration
+   *
+   * if NO_THREAD_LOCAL is defined, returns global instance
+   * otherwise, return thread-local instance
+   */
+  static T& get_instance() noexcept(std::is_nothrow_constructible_v<T>) {
+#ifdef NO_THREAD_LOCAL
+    return get_no_thread_local_instance();  // using non-thread-local version
+#else
+    return get_thread_local_instance();  // used thread-local version
+#endif  // NO_THREAD_LOCAL
+  }
+
+ protected:
+  /**
+   * @brief protected default constructor
+   *
+   * prevents external instatiation while allowing derived classes to construct
+   * marked noexcept to support environments where exceptions are disabled
+   */
+  Singleton() noexcept = default;
+
+  /**
+   * @brief virtual destructor for safe inheritance
+   *
+   * make sure correct destruction wheren deleting through a base class pointer
+   * pure virtual destructor make this class abstract
+   */
+  virtual ~Singleton() = default;
 };
 
 }  // namespace internal
